15e7845ce29f279e0de76a3f39a3db666fc08c34
Azazel5 <subhanga2013@gmail.com>
2021-01-02 01:07:40 -0500

    Finished the structures chapter; used concepts to write various programs such as table lookups or word finders (using binary trees)

--- C-linux/knr.c
+++ C-linux/knr.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /*
  * Just to be clear, a call by reference is passing a pointer to the function. Array arguments are 
@@ -123,7 +124,7 @@ void find_longest_line()
 */
 
 // Custom example of atoi
-int atoi(char s[])
+int atoi_(char s[])
 {
     // Note - s[i] - '0' gives the ASCII character value for the char stored in s[i]
     int n = 0;
@@ -169,7 +170,7 @@ char *squeeze(char *s, int c)
 // Try to find smarter ways to write boolean statements: a mixture of &&, ||, and ! makes things
 // unreadable.
 
-double atof(char s[])
+double atof_(char s[])
 {
     // Converts a string to a double
     double val, power;
@@ -293,7 +294,7 @@ char *alloc(int n)
     }
 }
 
-void free(char *p)
+void free_(char *p)
 {
     if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
         allocptr = p;
@@ -359,8 +360,327 @@ void free(char *p)
 */
 
 // You can also pass functions as pointers to other functions. Notice that int (*comp) {} and int *comp {}
-// have different meanings: the former means that comp is a pointer to a function that returns an int 
-// and the latter is that comp is a function which returns a pointer to an int. 
+// have different meanings: the former means that comp is a pointer to a function that returns an int
+// and the latter is that comp is a function which returns a pointer to an int.
+
+// Keeping a bunch of related variables into one component ~ a structure, which is extremely useful. Use
+// the dot operator to access variables inside the structs. Structures can be nested as well.
+
+// An example payroll struct
+struct date
+{
+    int month;
+    int day;
+    int year;
+    char month_name[4];
+};
+
+struct person
+{
+    char name[100];
+    char address[1000];
+    long zipcode;
+    long ss_number;
+    double salary;
+    struct date birth_information;
+    struct date hire_date;
+};
+
+// Structures cannot be passed to or returned from functions, and we can only get the address of or
+// the value of its members. However, "pointers to structures do not suffer from these limitations".
+// If you instead have a pointer to a structure, you'll have to use struct_name->variable_name to
+// access that variable. This is useful, as you won't have to declare all the arguments in the
+// function where you passed the pointer.
+
+// Defining an array of structs:
+// struct c {
+//  ...
+// } keytab[100];
+// Another way to write this: struct key keytab[100] -> defines an array keytab, which
+// holds 100 key structs.
+
+struct key
+{
+    char *keyword;
+    int keycount;
+} keytab[100];
+
+int binary_search(char *word, struct key tab[], int n)
+{
+    // the second argument is how you pass in an array (and thus a pointer) of structs to a function
+    int low = 0, mid = 0, high = n - 1, contd;
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        if ((contd = strcmp(word, tab[mid].keyword)) < 0)
+            high = mid - 1;
+        else if (contd < 0)
+            low = mid + 1;
+        else
+            return mid;
+    }
+
+    return -1;
+}
+
+// To find the size of a struct array, you can use the sizeof operator to divide each struct by
+// the space occupied by the total array i.e. sizeof(keytab) / sizeof(key)
+
+#define LETTER 'a'
+#define DIGIT '0'
+
+int type(int c)
+{
+    // Determines type of character, using the defines above: the defines themselves can be
+    // anything that is a digit or letter
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+        return LETTER;
+    else if (c >= '0' && c <= '9')
+        return DIGIT;
+    else
+        return c;
+}
+
+int getword(char *w, int lim)
+{
+    int c, t;
+
+    if (type(c = *w++ = getch()) != LETTER)
+    {
+        *w = '\0';
+        return c;
+    }
+
+    while (--lim > 0)
+    {
+        t = type(c = *w++ = getch());
+        if (t != LETTER && t != DIGIT)
+        {
+            ungetch(c);
+            break;
+        }
+    }
+
+    *(w - 1) = '\0';
+    return LETTER;
+}
+
+void countCKeywords()
+{
+    int n, t;
+    char word[20];
+
+    while ((t = getword(word, 20)) != EOF)
+        if (t == LETTER)
+            if ((n = binary_search(word, keytab, 100)) >= 0)
+                keytab[n].keycount++;
+
+    for (n = 0; n < 100; n++)
+        if (keytab[n].keycount > 0)
+            printf("%4d %s\n", keytab[n].keycount, keytab[n].keyword);
+}
+
+// We could've also written the above function using pointers to the struct array, with minor modifications.
+// The bianry search function would have to return pointers to each key struct element. In countCKeywords,
+// the increment would have to be done like n->keycount++, assuming we made the changes to binary_search
+// returns struct key *.
+
+// When functions are returning a complicated type like struct key *binary(...), an alternate style which can
+// be used is:
+// struct key *
+// binary(...)
+
+// Structs can also refer to themselves. Let's take the problem of trying to coutn occurrences of all
+// words in an input. We can use structs to implement binary trees to help us do that efficiently.
+// We'll make it so that the left subtree will only contain words smaller than the word at the node
+// and the right subtree will have the "greater" words.
+
+// Self declarations on structs are also legal, but there's a catch: the declaration must be a
+// pointer to the struct node for it to be okay.
+
+#define MAXWORD 20
+
+struct tnode
+{
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+};
+
+char *strsave(char *s)
+{
+    char *p;
+
+    if ((p = malloc(strlen(s) + 1)) != NULL)
+        strcpy(p, s);
+
+    return p;
+}
+
+// When you're allocating space for different data types, you must be mindful and use some
+// extra space to satisfy all alignment restrictions. This is why casting is done on
+// malloc/calloc calls.
+
+struct tnode *talloc()
+{
+    return (struct tnode *)alloc(sizeof(struct tnode));
+}
+
+struct tnode *tree(struct tnode *p, char *w)
+{
+    int cond;
+    if (p == NULL)
+    {
+        p = talloc();
+        p->word = strsave(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    }
+    else if ((cond = strcmp(w, p->word)) == 0)
+        p->word++;
+    else if (cond < 0)
+        p->left = tree(p->left, w);
+    else
+        p->right = tree(p->right, w);
+
+    return p;
+}
+
+void treeprint(struct tnode *p)
+{
+    if (p != NULL)
+    {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+void wordFrequencyCounter()
+{
+    struct tnode *root;
+    char word[MAXWORD];
+    int t;
+
+    root = NULL;
+    while ((t = getword(word, MAXWORD)) != EOF)
+        if (t == LETTER)
+            root = tree(root, word);
+
+    treeprint(root);
+}
+
+// Table lookup programs are pretty fundamental, as they're used heavily in areas hidden by
+// high level packages or programming languages. Eg. the #define A 1 call will put A and 1
+// in a table-like structure i.e. hashtable (dictionary for you python geeks out there).
+// Implementing this will be simple:
+// Hash the incoming name and use it to index into an array of pointers -> pointing to the description
+// of the names.
+
+#define HASHSIZE 100
+
+struct nlist
+{
+    char *name;
+    char *def;
+    struct nlist *next;
+} * hashtable[HASHSIZE];
+
+int hashfunction(char *s)
+{
+    int hashval;
+    for (hashval = 0; *s != '\0'; s++)
+        hashval += *s;
+
+    return hashval;
+}
+
+struct nlist *lookup(char *s)
+{
+    // Returns a pointer to the nlist entry if the name exists
+
+    struct nlist *np;
+    for (np = hashtable[hashfunction(s)]; np != NULL; np = np->next)
+        if (strcmp(np->name, s) == 0)
+            return np;
+
+    return NULL;
+}
+
+/*
+    Reading this book (and the intro to OS class) has taught me:
+    
+    1. Error checking is crucial. After any computation, always wrap your variables with a 
+       quick error check
+    2. Writing a quick paragraph detailing the steps you have to take for your function to 
+       work is very valuable i.e. the software design doc. Imagine you're a professor writing a 
+       project for your class. 
+*/
+
+struct nlist *install(char *name, char *def)
+{
+    struct nlist *np;
+    int hashval;
+
+    if ((np = lookup(name)) == NULL)
+    {
+        // No such record in the hash table
+        np = (struct nlist *)alloc(sizeof(*np));
+        if (np == NULL)
+            return NULL;
+
+        if ((np->name = strsave(name)) == NULL)
+            return NULL;
+
+        hashval = hashfunction(np->name);
+        np->next = hashtable[hashval];
+        hashtable[hashval] = np;
+    }
+    else
+    {
+        // Found the record, np now is the pointer to the record
+        free(np->def);
+    }
+
+    // Free any previous definition, even if a name was found in the table, so it can be overwritten
+    // by this call below
+    if ((np->def = strsave(def)) == NULL)
+        return NULL;
+
+    return np;
+}
+
+// Symbol tables in compilers must know whether the text is a keyword, static, or external. The usual way
+// this is done is define some masks which are bit positions, and turn them off or on using bitwise
+// operations. Eg. flags |= EXTERNAL | STATIC to turn on or flags &= ~(EXTERNAL | STATIC).
+// if ((flags & (EXTERNAL | STATIC)) == 0) -> will be true if both bits are off
+// You could also define a set of bits within an int; the bits are adjacent and are 1 bit each.
+// These are called fields.
+
+struct
+{
+    unsigned is_keyword : 1;
+    unsigned is_extern : 1;
+    unsigned is_static : 1;
+} flags;
+
+// Unions are a type of structure that can hold variables of different types. The union is big enough
+// to hold the variable of the "widest" type. Another useful tool we have got as C programmers
+// is the typedef facility. The typedef occurs after the variable type i.e. typdef char *reverendDonkBonkers.
+
+// Here's a good example, using the tree nodes shown earlier
+typedef struct tnode
+{
+    char *word;
+    int count;
+    struct tnode *left;
+    struct tnode *right;
+} TREENODE, *TREEPTR;
+
+// Why do this? This will give you the structure and a pointer to the structure too. Using this,
+// you'll be able to pass in the TREEPTR in the alloc call too: you'll get more flexibility and
+// readability.
 
 int main()
 {
